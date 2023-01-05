0e9bf35a810a8efbb36300c7d8e4e8934d752d35
jona799t <jonathan.b.skettrup@gmail.com>
2023-01-05 19:49:40 +0100

    Credentials ændring

--- api/databaseHandler.py
+++ api/databaseHandler.py
@@ -3,7 +3,18 @@ import time
 import os
 import json
 
-credentials = json.loads(os.environ.get("credentials"))
+credentials = {
+    'type': os.environ.get("type"),
+    'project_id': os.environ.get("project_id"),
+    'private_key_id': os.environ.get("private_key_id"),
+    'private_key': os.environ.get("private_key"),
+    'client_email': os.environ.get("client_email"),
+    'client_id': os.environ.get("client_id"),
+    'auth_uri': os.environ.get("auth_uri"),
+    'token_uri': os.environ.get("token_uri"),
+    'auth_provider_x509_cert_url': os.environ.get("auth_provider_x509_cert_url"),
+    'client_x509_cert_url': os.environ.get("type")
+}
 
 serviceAccount = gspread.service_account_from_dict(credentials)
 database = serviceAccount.open("Better Lectio")
